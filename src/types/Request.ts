210830cb6838214df4b6c8f4a14498d3f02cd9a0
RanR112 <146650544+RanR112@users.noreply.github.com>
2025-04-07 03:18:52 +0700

    Middleware Verify Token

---
+++ src/types/Request.ts
@@ -0,0 +1,12 @@
+import { Request } from "express"
+
+type User = {
+    id: string,
+    name: string,
+    role: "admin" | "customer",
+    email: string
+}
+
+export interface CustomRequest extends Request {
+    user?: User;
+}
