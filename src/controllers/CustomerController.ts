bea1e4f0093099df07e723b7241208edef6314c8
RanR112 <146650544+RanR112@users.noreply.github.com>
2025-04-06 23:49:43 +0700

    API Get WalletTransaction

--- src/controllers/CustomerController.ts
+++ src/controllers/CustomerController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import WalletTransaction from "../models/WalletTransaction";
 
 export const getCustomer = async (req: Request, res: Response) => {
     try {
@@ -7,6 +8,31 @@ export const getCustomer = async (req: Request, res: Response) => {
 
         return res.json({
             data: customers,
+            message: "Success get data",
+            status: "Success"
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            message: "Failed to get data",
+            data: null,
+            status: "Failed"
+        })
+    }
+}
+
+export const getWalletTransactions = async (req: Request, res: Response) => {
+    try {
+        const transactions = await WalletTransaction.find().populate({
+            path: "wallet",
+            select: "user -_id",
+            populate: {
+                path: "user",
+                select: "name"
+            }
+        })
+        return res.json({
+            data: transactions,
             message: "Success get data",
             status: "Success"
         })
--- src/routes/adminRoutes/customerRoutes.ts
+++ src/routes/adminRoutes/customerRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getCustomer } from "../../controllers/CustomerController";
+import { getCustomer, getWalletTransactions } from "../../controllers/CustomerController";
 
 const customerRoutes = express.Router();
 
 customerRoutes.get("/customers", getCustomer);
+customerRoutes.get("/wallet-transactions", getWalletTransactions);
 
 export default customerRoutes;
