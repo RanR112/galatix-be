d517ead8470045fa4bb2a5093dfe8f34ed25702d
RanR112 <146650544+RanR112@users.noreply.github.com>
2025-04-08 01:37:27 +0700

    API for Detail Page

--- src/controllers/GlobalController.ts
+++ src/controllers/GlobalController.ts
@@ -40,3 +40,59 @@ export const getGenre = async (req: Request, res: Response) => {
         })
     }
 }
+
+export const getMovieDetail = async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params;
+
+        const seats = []
+
+        for (let i = 0; i < 5; i++) {
+            seats.push({
+                seat: `A${i + 1}`,
+                isBooked: false
+            })
+        }
+
+        for (let i = 0; i < 5; i++) {
+            seats.push({
+                seat: `B${i + 1}`,
+                isBooked: false
+            })
+        }
+
+        for (let i = 0; i < 5; i++) {
+            seats.push({
+                seat: `C${i + 1}`,
+                isBooked: false
+            })
+        }
+
+        const movie = await Movie.findById(id).populate({
+            path: 'theaters',
+            select: 'name city'
+        }).populate({
+            path: "genre",
+            select: "name -_id"
+        })
+
+        return res.json({
+            data: {
+                movie: {
+                    ...movie?.toJSON(),
+                    seats,
+                    times: ["12:30", "14:50", "18:30", "22:30", "23:30"],
+                },
+            },
+            message: "Success get data",
+            status: "Success"
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to get data",
+            data: null,
+            status: "Failed"
+        })
+    }
+}
--- src/routes/customerRoutes/globalRoutes.ts
+++ src/routes/customerRoutes/globalRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getGenre, getMovies } from "../../controllers/GlobalController";
+import { getGenre, getMovieDetail, getMovies } from "../../controllers/GlobalController";
 
 const globalRoutes = express.Router();
 
 globalRoutes.get('/movies', getMovies)
 globalRoutes.get("/genres", getGenre)
+globalRoutes.get("/movies/:id", getMovieDetail)
 
 export default globalRoutes
