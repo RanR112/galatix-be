137dc73d5c64699f27441cf509ca93732f5c1eeb
RanR112 <146650544+RanR112@users.noreply.github.com>
2025-04-07 16:13:50 +0700

    API Register

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,6 +3,7 @@ import { authSchema } from "../utils/zodSchema";
 import User from "../models/User";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import Wallet from "../models/Wallet";
 
 export const login = async (req: Request, res: Response) => {
     try {
@@ -42,7 +43,7 @@ export const login = async (req: Request, res: Response) => {
         }, secretKey, {expiresIn: '24h'})
 
         return res.json({
-            message: "Success login",
+            message: "Success to login",
             data: {
                 name: checkUser.name,
                 email: checkUser.email,
@@ -53,9 +54,72 @@ export const login = async (req: Request, res: Response) => {
             status: "Success"
         })
     } catch (error) {
-        console.log(error)
         return res.status(500).json({
-            message: "Failed login",
+            message: "Failed to login",
+            data: null,
+            status: "Failed"
+        })
+    }
+}
+
+export const register = async (req: Request, res: Response) => {
+    try {
+        const parse = authSchema.omit({
+            role: true
+        }).safeParse(req.body)
+
+        if (!parse.success) {
+            const errorMessages = parse.error.issues.map((err) => err.message) 
+
+            return res.status(400).json({
+                message: "Invalid request",
+                data: errorMessages,
+                status: "failed"
+            })
+        }
+
+        const emailExisted = await User.findOne({
+            email: parse.data.email
+        })
+
+        if (emailExisted) {
+            return res.status(400).json({
+                message: "Email already exist",
+                data: null,
+                status: "failed"
+            })
+        }
+
+        const hashPassword = bcrypt.hashSync(parse.data.password, 12)
+
+        const user = new User({
+            name: parse.data.name,
+            email: parse.data.email,
+            password: hashPassword,
+            role: "customer",
+            photo: req.file?.filename
+        })
+
+        const wallet = new Wallet({
+            balance: 0,
+            user: user._id
+        })
+
+        await user.save()
+        await wallet.save()
+
+        return res.json({
+            message: "Success sign up",
+            data: {
+                name: user.name,
+                email: user.email
+            },
+            status: "success"
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to register",
             data: null,
             status: "Failed"
         })
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,18 @@
 import express from 'express';
-import { login } from '../controllers/AuthController';
+import { login, register } from '../controllers/AuthController';
 import { validateRequest } from '../middlewares/validateRequest';
 import { authSchema } from '../utils/zodSchema';
+import { imageFilter, thumbnailStorage } from '../utils/multer';
+import multer from 'multer';
 
 const authRoutes = express.Router();
 
+const upload = multer({
+    storage: thumbnailStorage("public/uploads/photos"),
+    fileFilter: imageFilter
+})
+
 authRoutes.post('/auth/login', validateRequest(authSchema.omit({name: true})), login)
+authRoutes.post('/auth/register', upload.single("photo"), register)
 
 export default authRoutes
