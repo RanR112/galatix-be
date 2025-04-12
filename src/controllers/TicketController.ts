759879f26b0a9f89d576575ff32b5cad8e8cd2ab
RanR112 <146650544+RanR112@users.noreply.github.com>
2025-04-13 03:26:40 +0700

    API for Transaction Ticket

---
+++ src/controllers/TicketController.ts
@@ -0,0 +1,69 @@
+import type { Response } from "express";
+import type { CustomRequest } from "../types/Request";
+import { transactionSchema } from "../utils/zodSchema";
+import Wallet from "../models/Wallet";
+import Transaction from "../models/Transaction";
+import TransactionSeat from "../models/TransactionSeat";
+
+export const transactionTicket = async (req: CustomRequest, res: Response) => {
+    try {
+        const parse = transactionSchema.parse(req.body)
+
+        const wallet = await Wallet.findOne({
+            user: req.user?.id
+        })
+
+        if (!wallet || (wallet && wallet.balance < parse.total)) {
+            return res.status(400).json({
+                status: "failed",
+                message: "Insufficient balance. please top up your balance first",
+                data: null
+            })
+        }
+
+        const transaction = new Transaction({
+            bookingFee: parse.bookingFee,
+            total: parse.total,
+            subtotal: parse.subtotal,
+            theater: parse.theaterId,
+            movie: parse.movieId,
+            tax: parse.tax,
+            user: req.user?.id,
+            date: parse.date
+        })
+
+        for (const seat of parse.seats) {
+            const newSeat = new TransactionSeat({
+                transaction: transaction.id,
+                seat: seat
+            })
+
+            await newSeat.save()
+        }
+
+        const transactionSeats = await TransactionSeat.find({
+            transaction: transaction.id
+        })
+
+        transaction.seats = transactionSeats.map((va) => va._id)
+
+        const currBallance = wallet.balance ?? 0
+
+        await Wallet.findByIdAndUpdate(wallet.id, {
+            balance: currBallance - parse.total
+        })
+
+        await transaction.save()
+
+        return res.json({
+            message: "Success transaction ticket",
+            status: "success",
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to transaction ticket",
+            data: null,
+            status: "Failed",
+        });
+    }
+}
--- src/routes/customerRoutes/globalRoutes.ts
+++ src/routes/customerRoutes/globalRoutes.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import { getAvailableSeats, getGenre, getMovieDetail, getMovies, getMoviesFilter } from "../../controllers/GlobalController";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { transactionSchema } from "../../utils/zodSchema";
+import { transactionTicket } from "../../controllers/TicketController";
 
 const globalRoutes = express.Router();
 
@@ -8,5 +11,6 @@ globalRoutes.get("/genres", getGenre)
 globalRoutes.get("/movies/:id", getMovieDetail)
 globalRoutes.get("/check-seats/:movieId", getAvailableSeats)
 globalRoutes.get("/browse-movies/:genreId", getMoviesFilter)
+globalRoutes.post("/transaction/buy", validateRequest(transactionSchema), transactionTicket)
 
 export default globalRoutes
