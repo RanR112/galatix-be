ddd037ade8c3c88591ad03be1a53f9ee66de203b
RanR112 <146650544+RanR112@users.noreply.github.com>
2025-04-13 02:32:52 +0700

    API for Get Topup Hisotry

--- src/controllers/WalletController.ts
+++ src/controllers/WalletController.ts
@@ -1,25 +1,50 @@
 import type { Response } from "express";
 import Wallet from "../models/Wallet";
 import type { CustomRequest } from "../types/Request";
+import WalletTransaction from "../models/WalletTransaction";
 
 export const getBalance = async (req: CustomRequest, res: Response) => {
     try {
         const wallet = await Wallet.findOne({
-            user: req.user?.id
-        })
+            user: req.user?.id,
+        });
 
         return res.json({
             status: true,
             message: "Success get data",
             data: {
-                balance: wallet?.balance ?? 0
-            }
-        })
+                balance: wallet?.balance ?? 0,
+            },
+        });
     } catch (error) {
         return res.status(500).json({
             message: "Failed to get data",
             data: null,
-            status: "Failed"
-        })
+            status: "Failed",
+        });
     }
-}
+};
+
+export const getTopupHistory = async (req: CustomRequest, res: Response) => {
+    try {
+        const wallet = await Wallet.findOne({
+            user: req.user?.id,
+        });
+
+        const data = await WalletTransaction.find({
+            wallet: wallet?._id,
+        }).select("wallet price createdAt status");
+
+        return res.json({
+            status: true,
+            message: "Success get data",
+            data: data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to get data",
+            data: null,
+            status: "Failed",
+        });
+    }
+};
--- src/routes/customerRoutes/walletRoutes.ts
+++ src/routes/customerRoutes/walletRoutes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getBalance } from '../../controllers/WalletController';
+import { getBalance, getTopupHistory } from '../../controllers/WalletController';
 
 const walletRoutes = express.Router()
 
 walletRoutes.get("/check-balance", getBalance)
+walletRoutes.get("/topup-history", getTopupHistory)
 
 export default walletRoutes
